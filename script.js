84df9bcfad642851d30e4762ddf25125168ce0ba
Sozai83 <chikushi3.227@gmail.com>
2022-11-05 13:59:01 +1100

    clean up comments in js file

--- script.js
+++ script.js
@@ -3,23 +3,23 @@ const list = document.getElementById('list');
 const input = document.getElementById('todo');
 const deleteButtons = document.querySelectorAll('.delete');
 
-//2. Get what's in the textbox when Add is clicked
+//Get what's in the textbox when Add is clicked
 const getToDoItem = (el)=>{
     return el.value;
 }
 
-// 3. Add todo list item in the DOM when Add is clicked
+//Add todo list item in the DOM when Add is clicked
 const addHTML = (text)=>{
     let html = '<li class="list-item"><span class="list-text">'+text+'</span><i class="fas fa-trash-alt delete"></i></li>';
     list.insertAdjacentHTML("afterbegin", html);
 }
 
-// 4. Remove the value from the text 
+//Remove the value from the text 
 const emptyInput = ()=>{
     input.value="";
 }
 
-// 5. Create a function
+//Create a function
 const addToDoItem = ()=>{
     let text = getToDoItem(input);
     if(text.length > 0){
