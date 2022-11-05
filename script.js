f8474209e4d8f5970fe48273c496d1717fbce860
Sozai83 <chikushi3.227@gmail.com>
2022-11-05 13:56:19 +1100

    added functions to add/remove todo items from the list

--- script.js
+++ script.js
@@ -1,19 +1,53 @@
-//print what's in the textbox to the console
-// 1. detect when add button is pressed
 const add = document.getElementById('submit');
 const list = document.getElementById('list');
+const input = document.getElementById('todo');
+const deleteButtons = document.querySelectorAll('.delete');
 
 //2. Get what's in the textbox when Add is clicked
-const getToDoItem = ()=>{
-    let todoText = document.getElementById('todo');
-    return todoText.value;
+const getToDoItem = (el)=>{
+    return el.value;
 }
 
 // 3. Add todo list item in the DOM when Add is clicked
-const addToDoItem = ()=>{
-    let text = getToDoItem();
-    let node = '<li class="list-item"><span class="list-text">'+text+'</span><i class="fas fa-trash-alt"></i></li>';
-    list.insertAdjacentHTML("afterbegin", node);
+const addHTML = (text)=>{
+    let html = '<li class="list-item"><span class="list-text">'+text+'</span><i class="fas fa-trash-alt delete"></i></li>';
+    list.insertAdjacentHTML("afterbegin", html);
 }
 
+// 4. Remove the value from the text 
+const emptyInput = ()=>{
+    input.value="";
+}
+
+// 5. Create a function
+const addToDoItem = ()=>{
+    let text = getToDoItem(input);
+    if(text.length > 0){
+        addHTML(text);
+        emptyInput();
+    }
+}
+
+// Remove element
+const removeToDoItem = el =>{
+    let item = el.parentNode;
+    item.remove();
+}
+
+//When Add button is clicked, add a todo Item
 add.addEventListener('click',addToDoItem);
+
+//When Enter button is pressed in input, trigger clicking Add button
+input.addEventListener('keypress',e =>{
+    if(e.key === "Enter"){
+        e.preventDefault();
+        add.click();
+    }
+});
+// When delete button is clicked, remove the todo item from the list
+list.addEventListener('click', (e) =>{
+    let target = e.target;
+    if(target.classList.contains('delete')){
+        removeToDoItem(e.target);
+    }
+});
