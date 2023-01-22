dbb953102fb52ce194807ed92d8e172d17a2391f
Shiori Chiku <chikushi3.227@gmail.c.om>
2023-01-22 21:39:13 +1100

    added addNote function

--- script.js
+++ script.js
@@ -3,8 +3,36 @@ const addNoteBtn = document.getElementById('add-note-input');
 const inputTitle = document.getElementsByClassName('input-note-title');
 const inputBody = document.getElementsByClassName('input-note-body')
 
+const noteList = [];
 
-// Get the value of title and the text
+class Note {
+    constructor() {
+      this.title = 'title';
+      this.body = 'body'
+    }
+}
+
+const addNote = function(){
+    //  Get the value of title and the text
+    let title = inputTitle[0].value;
+    let body = inputBody[0].value;
+    if (title.length != 0 && body.length != 0){
+        // If the noteList is empty, return note-1
+        // If not, it will take the id of the last element and add 1 (i.e. last id = note-2 => note-3)
+        let id = noteList.length == 0 ? 
+        'note-1' :
+        'note-' + (parseInt(noteList[noteList.length-1].id.slice(noteList[noteList.length-1].id.indexOf('-') + 1)) + 1);
+
+        // Creates tempObj of Note
+        let tempObj = new Note();
+        tempObj.title = title;
+        tempObj.body = body;
+        tempObj.id = id;
+
+        // Push the tempObj at the end of the array
+        noteList.push(tempObj);
+    }
+}
 
 // Clear the value of title and the text
 const clearNote = function(){
@@ -15,4 +43,4 @@ const clearNote = function(){
 
 // Ditect when clear and add button is clicked
 clearNoteBtn.addEventListener('click', clearNote);
-addNoteBtn.addEventListener('click', function(){console.log('Add Note button is clicked')});
+addNoteBtn.addEventListener('click', addNote);
