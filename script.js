350c731f8e74881dd2965bb3430f2ff13226c4c8
Shiori Chiku <chikushi3.227@gmail.c.om>
2023-01-22 21:04:18 +1100

    add functions to clear inputs

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-document.onclick= function(event) {
-    // Compensate for IE<9's non-standard event model
-    //
-    if (event===undefined) event= window.event;
-    var target= 'target' in event? event.target : event.srcElement;
+const clearNoteBtn = document.getElementById('clear-note-input');
+const addNoteBtn = document.getElementById('add-note-input');
+const inputTitle = document.getElementsByClassName('input-note-title');
+const inputBody = document.getElementsByClassName('input-note-body')
 
-    alert('clicked on '+target.className);
-};
+
+// Get the value of title and the text
+
+// Clear the value of title and the text
+const clearNote = function(){
+    inputTitle[0].value = '';
+    inputBody[0].value = '';
+}
+
+
+// Ditect when clear and add button is clicked
+clearNoteBtn.addEventListener('click', clearNote);
+addNoteBtn.addEventListener('click', function(){console.log('Add Note button is clicked')});
